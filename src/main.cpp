3ea4a937de740b3492bfc2a3e11b5b7e99f38437
Gianluca D <gianluca.delfino@gmail.com>
2013-11-07 23:24:58 +0000

    static linked release version

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main ( int argc, char **argv )
 	if( SDL_Init( SDL_INIT_EVERYTHING ) ) //returns 0 if everything is ok!
 	{
 		std::cerr << "Could NOT initialize SDL. Error: " << SDL_GetError() << std::endl;
+		exit(EXIT_FAILURE);
 	}
 
 	SDL_WM_SetCaption( "2D Physics Engine. Gianluca.Delfino@gmail.com", NULL );
