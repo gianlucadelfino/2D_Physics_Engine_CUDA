34f8334aa57ddeee19a3e8e05c0953e979caa7ca
Gianluca D <gianluca.delfino@gmail.com>
2015-04-11 00:07:57 +0100

    tabs to spaces

--- src/CSceneMainMenu.cpp
+++ src/CSceneMainMenu.cpp
@@ -6,85 +6,85 @@
 #include "CUDA_utils.cuh"
 
 CSceneMainMenu::CSceneMainMenu( SDL_Surface* screen_, CWorld& world_ ):
-	IScene( screen_, world_, SDL_MapRGB( screen_->format, 0, 0, 0 ) )
+    IScene( screen_, world_, SDL_MapRGB( screen_->format, 0, 0, 0 ) )
 {}
 
 CSceneMainMenu::CSceneMainMenu( const CSceneMainMenu& other_ ):
-	IScene( other_ )
+    IScene( other_ )
 {}
 
 CSceneMainMenu& CSceneMainMenu::operator=( const CSceneMainMenu& rhs )
 {
-	if ( this != &rhs )
-	{
-		IScene::operator=( rhs );
-	}
-	return *this;
+    if ( this != &rhs )
+    {
+        IScene::operator=( rhs );
+    }
+    return *this;
 }
 
 void CSceneMainMenu::Init()
 {
-	//add UI elements to Scene
-	Uint32 white_color = SDL_MapRGB( this->mp_screen->format, 255, 255, 255 );
-	Uint32 black_color = SDL_MapRGB( this->mp_screen->format, 0, 0, 0 );
-	SDL_Color button_label_color = { 0, 0, 0, 0 };
+    //add UI elements to Scene
+    Uint32 white_color = SDL_MapRGB( this->mp_screen->format, 255, 255, 255 );
+    Uint32 black_color = SDL_MapRGB( this->mp_screen->format, 0, 0, 0 );
+    SDL_Color button_label_color = { 0, 0, 0, 0 };
 
-	//TITLE label
-	std::shared_ptr< CFont > title_font( new CFont( "pcseniorSmall.ttf", 30 ) );
-	SDL_Color title_color = { 255, 255, 255, 0 };
-	std::unique_ptr< CMoveableButton > title_moveable( new CMoveableButton( C2DVector( 400.0f, 50.0f ), C2DVector( 300.0f, 31.0f ) ) );
-	std::unique_ptr< CDrawableButton > title_drawable( new CDrawableButton( title_font, this->mp_screen, "SIMULINO 2000", C2DVector( 200.0f, 31.0f ), black_color, title_color ) );
+    //TITLE label
+    std::shared_ptr< CFont > title_font( new CFont( "pcseniorSmall.ttf", 30 ) );
+    SDL_Color title_color = { 255, 255, 255, 0 };
+    std::unique_ptr< CMoveableButton > title_moveable( new CMoveableButton( C2DVector( 400.0f, 50.0f ), C2DVector( 300.0f, 31.0f ) ) );
+    std::unique_ptr< CDrawableButton > title_drawable( new CDrawableButton( title_font, this->mp_screen, "SIMULINO 2000", C2DVector( 200.0f, 31.0f ), black_color, title_color ) );
 
-	std::unique_ptr< CEntity > title( new CEntity(
-		1, std::move( title_moveable ),
-		std::move( title_drawable )
-		));
-	this->m_UI_elements.push_back( std::move( title ) );
+    std::unique_ptr< CEntity > title( new CEntity(
+        1, std::move( title_moveable ),
+        std::move( title_drawable )
+        ));
+    this->m_UI_elements.push_back( std::move( title ) );
 
-	//switch to CLOTH simulation button
-	std::shared_ptr< CFont > button_font( new CFont( "pcseniorSmall.ttf", 20 ) );
+    //switch to CLOTH simulation button
+    std::shared_ptr< CFont > button_font( new CFont( "pcseniorSmall.ttf", 20 ) );
 
-	std::unique_ptr< CMoveableButton > cloth_moveable_button( new CMoveableButton( C2DVector( 500.0f, 300.0f ), C2DVector( 200.0f, 22.0f ) ) );
-	std::unique_ptr< CDrawableButton > cloth_button_drawable( new CDrawableButton( button_font, this->mp_screen, "CLOTH", C2DVector( 200.0f, 22.0f ), white_color, button_label_color ) );
+    std::unique_ptr< CMoveableButton > cloth_moveable_button( new CMoveableButton( C2DVector( 500.0f, 300.0f ), C2DVector( 200.0f, 22.0f ) ) );
+    std::unique_ptr< CDrawableButton > cloth_button_drawable( new CDrawableButton( button_font, this->mp_screen, "CLOTH", C2DVector( 200.0f, 22.0f ), white_color, button_label_color ) );
 
-	std::unique_ptr< CEntity > cloth_sim_switch_button( new CEntityButton(
-		1, std::move( cloth_moveable_button ),
-		std::move( cloth_button_drawable ),
-		*this->mr_world,
-		std::move( std::unique_ptr< IScene >( new CSceneCloth( this->mp_screen, *this->mr_world) ) )
-		));
-	this->m_UI_elements.push_back( std::move( cloth_sim_switch_button ) );
+    std::unique_ptr< CEntity > cloth_sim_switch_button( new CEntityButton(
+        1, std::move( cloth_moveable_button ),
+        std::move( cloth_button_drawable ),
+        *this->mr_world,
+        std::move( std::unique_ptr< IScene >( new CSceneCloth( this->mp_screen, *this->mr_world) ) )
+        ));
+    this->m_UI_elements.push_back( std::move( cloth_sim_switch_button ) );
 
-	//switch to galaxy simulation button
-	std::unique_ptr< CMoveableButton > moveable_button( new CMoveableButton( C2DVector( 500.0f, 400.0f ), C2DVector(  200.0f, 22.0f ) ) );
-	std::unique_ptr< CDrawableButton > galaxy_button_drawable( new CDrawableButton( button_font, this->mp_screen, "GALAXY", C2DVector(  200.0f, 22.0f ), white_color, button_label_color ) );
+    //switch to galaxy simulation button
+    std::unique_ptr< CMoveableButton > moveable_button( new CMoveableButton( C2DVector( 500.0f, 400.0f ), C2DVector(  200.0f, 22.0f ) ) );
+    std::unique_ptr< CDrawableButton > galaxy_button_drawable( new CDrawableButton( button_font, this->mp_screen, "GALAXY", C2DVector(  200.0f, 22.0f ), white_color, button_label_color ) );
 
-	/*The (-1) on the initial_stars_num accounts for the possible presence of the extra star when holding down the button.
-	Like so, the CUDA kernel will run the same amount of blocks, even if we add another star, and there is no performance hit.*/
-	unsigned int initial_stars_num = 8*1024 - 1;  //assume CUDA
-	bool start_in_cuda_mode = true;
-	//check CUDA Availability
-	if ( !CUDA_utils::IsCUDACompatibleDeviceAvailable() )
-	{
-		initial_stars_num = 512;
-		start_in_cuda_mode = false;
-	}
+    /*The (-1) on the initial_stars_num accounts for the possible presence of the extra star when holding down the button.
+    Like so, the CUDA kernel will run the same amount of blocks, even if we add another star, and there is no performance hit.*/
+    unsigned int initial_stars_num = 16*1024 - 1;  //assume CUDA
+    bool start_in_cuda_mode = true;
+    //check CUDA Availability
+    if ( !CUDA_utils::IsCUDACompatibleDeviceAvailable() )
+    {
+        initial_stars_num = 512;
+        start_in_cuda_mode = false;
+    }
 
-	std::unique_ptr< CEntity > galaxy_sim_switch_button( new CEntityButton(
-		1, std::move( moveable_button ),
-		std::move( galaxy_button_drawable ),
-		*this->mr_world,
-		std::move( std::unique_ptr< IScene >( new CSceneGalaxy( this->mp_screen, *this->mr_world, start_in_cuda_mode, initial_stars_num ) ) )
-		));
-	this->m_UI_elements.push_back( std::move( galaxy_sim_switch_button ) );
+    std::unique_ptr< CEntity > galaxy_sim_switch_button( new CEntityButton(
+        1, std::move( moveable_button ),
+        std::move( galaxy_button_drawable ),
+        *this->mr_world,
+        std::move( std::unique_ptr< IScene >( new CSceneGalaxy( this->mp_screen, *this->mr_world, start_in_cuda_mode, initial_stars_num ) ) )
+        ));
+    this->m_UI_elements.push_back( std::move( galaxy_sim_switch_button ) );
 
-	//credits label
-	std::unique_ptr< CMoveableButton > credits_moveable( new CMoveableButton( C2DVector( 800.0f, 660.0f ), C2DVector( 300.0f, 22.0f ) ) );
-	std::unique_ptr< CDrawableButton > credits_drawable( new CDrawableButton( button_font, this->mp_screen, "by Gianluca Delfino", C2DVector( 300.0f, 22.0f ), black_color, title_color ) );
+    //credits label
+    std::unique_ptr< CMoveableButton > credits_moveable( new CMoveableButton( C2DVector( 800.0f, 660.0f ), C2DVector( 300.0f, 22.0f ) ) );
+    std::unique_ptr< CDrawableButton > credits_drawable( new CDrawableButton( button_font, this->mp_screen, "by Gianluca Delfino", C2DVector( 300.0f, 22.0f ), black_color, title_color ) );
 
-	std::unique_ptr< CEntity > credits( new CEntity(
-		1, std::move( credits_moveable ),
-		std::move( credits_drawable )
-		));
-	this->m_UI_elements.push_back( std::move( credits ) );
+    std::unique_ptr< CEntity > credits( new CEntity(
+        1, std::move( credits_moveable ),
+        std::move( credits_drawable )
+        ));
+    this->m_UI_elements.push_back( std::move( credits ) );
 }
--- src/headers/device_compute_grav_forces.h
+++ src/headers/device_compute_grav_forces.h
@@ -6,15 +6,17 @@
 
 /**
 * compute_grav computes the forces acting on each stars.
-* @param h_pos the pointer to the host vector holding the current positions of the stars
+* @param h_pos_x the pointer to the host vector holding the current x positions of the stars
+* @param h_pos_y the pointer to the host vector holding the current y positions of the stars
 * @param h_masses the pointer to the host vector holding all the masses of the stars
 * @param h_gravs the pointer to the host vector that is going to be filled with the forces
 * @param num_stars is the total number of the stars
 */
 void compute_grav(
-	float2* h_pos,
-	float* h_masses,
-	float2* h_gravs,
-	unsigned int num_stars);
+    float* h_pos_x,
+    float* h_pos_y,
+    float* h_masses,
+    float2* h_gravs,
+    unsigned int num_stars);
 
 #endif
